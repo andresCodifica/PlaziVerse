d4d9fca5c53c654f3bcbc35c5e1180e18301885b
Andres Escobar <39176588+andresCodifica@users.noreply.github.com>
2021-05-18 22:33:00 -0500

    implement db module

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,17 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgenteModel = require('./models/agente')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgenteModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+  await sequelize.authenticate()
   const Agent = {}
   const Metric = {}
 
